87ee9f49bf1f4fde4a55c7fd6f48a2395ecb3255
muckystack <MiguelMartinezDW@outlook.com>
2019-09-03 20:49:05 -0500

    Agrego la imagen y el titulo asi como su subtitul

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
-  const BasicoPage({Key key}) : super(key: key);
+
+  final estiloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+  final estiloSubtitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Basico page'),
+      body: Column(
+        children: <Widget>[
+
+          Image(
+            image: NetworkImage('https://i.ytimg.com/vi/BfCwN4iy6T8/maxresdefault.jpg'),
+          ),
+
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+            child: Row(
+              children: <Widget>[
+                
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Cascada de agua', style: estiloTitulo,),
+                      SizedBox(height: 7.0),
+                      Text('La cascada se encuantra en alemania', style: estiloSubtitulo),
+                    ],
+                  ),
+                ),
+
+                Icon(Icons.star, color: Colors.red, size: 30.0),
+
+                Text('41', style: TextStyle(fontSize: 20.0))
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
 }
