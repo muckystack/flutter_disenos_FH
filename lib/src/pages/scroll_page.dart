02265948d950cdce4b7b36751f4ef955b0b758ba
muckystack <MiguelMartinezDW@outlook.com>
2019-09-03 22:59:11 -0500

    Agrego color e imagen de fondo. Tambien agrego una imagen para poner de baner y una imagen para poner en la pantalla 1

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -17,14 +17,36 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _pagina1() {
-    return Center(
-      child: Text('Pagina 1'),
+    return Stack(
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo()
+      ],
+    );
+  }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(39, 44, 77, 1.0),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage(''),
+        fit: BoxFit.cover,
+      ),
     );
   }
 
   Widget _pagina2() {
     return Center(
-      child: Text('Pagina 2'),
+      child: Text('Page 2'),
     );
   }
 }
